97c4f1c0668027779576a95e062fcc15d9dd1242
inventor01 <austinrock2000@gmail.com>
2017-06-30 22:39:43 -0400

    Add files via upload

---
+++ docs/happysmile.js
@@ -0,0 +1,20 @@
+function on1(){
+  document.getElementById("overlay1").style.display = "block";
+}
+
+function off1() {
+    document.getElementById("overlay1").style.display = "none";
+}
+
+function buttonabout(){
+window.alert();
+
+}
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
